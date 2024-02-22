b0c17c2fb7b78e782e04c2dbe1e6aa2f9534cb24
AwabElAdam <a.awab@saphiredent.com>
2024-02-22 13:15:43 +0300

    new services section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   theme: {
 
     extend: {
+      colors:{
+        'Saphire':{
+          Orange:"#F5862C",
+          Blue:"#2A3C47",
+        }
+      },
       fontFamily: {
         'sans': ["inter",'ui-sans-serif', 'system-ui'],
         'serif': ['ui-serif', 'Georgia'],
@@ -12,6 +18,10 @@ module.exports = {
         'body': ['"Open Sans"'],},
       backgroundImage: {
         'hero': "url('../assets/img/hero.webp')",
+        'smile': "url('../assets/img/hero.webp')",
+        'implant': "url('../assets/img/hero.webp')",
+        'crown': "url('../assets/img/hero.webp')",
+        'treatment': "url('../assets/img/hero.webp')",
       }
     },
   },
